e3e16f8c0f552f788740b8a9a3b5d695fd071389
Aishwary Tiwari <aishwary.tiwari@wdadc.com>
2017-08-25 17:57:28 +0530

    rest api controller

---
+++ application/controllers/Angularapi.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+require APPPATH . '/libraries/REST_Controller.php';
+
+// use namespace
+use Restserver\Libraries\REST_Controller;
+/**
+*
+* @package         CodeIgniter
+* @subpackage      Rest Server
+* @category        Controller
+
+ */
+Class Angularapi extends REST_Controller {
+
+
+
+}
