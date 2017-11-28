0c2740d6c509ec5874d9de7a299c5d27caa4bff9
Aishwary <aishwaryt92@gmail.com>
2017-11-28 19:38:21 +0530

    Update Angularapi.php

--- application/controllers/Angularapi.php
+++ application/controllers/Angularapi.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
-require APPPATH . '/libraries/REST_Controller.php';
+require APPPATH . 'libraries\REST_Controller.php';
 
 // use namespace
 use Restserver\Libraries\REST_Controller;
@@ -15,6 +15,34 @@ use Restserver\Libraries\REST_Controller;
  */
 Class Angularapi extends REST_Controller {
 
+	function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        // Configure limits on our controller methods
+        // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
+    	
+    }
+    public function index_get()
+    {
+      $this->set_response(['status' => FALSE,'message' => 'Data will be pass through POST method'], REST_Controller::HTTP_OK);
 
+    }
 
+     public function index_post()
+    {
+
+        $request_id = (int) $this->post('request_id');
+       
+          $params = new stdClass();
+         switch($request_id)
+         {
+              // request_id 0 bad request 
+               case 0:
+               $this->set_response(['response' => 201,'message' => 'BAD REQUEST'], REST_Controller::HTTP_OK); 
+               break;
+               // login 
+
+           }
+       }
 }
